f34e385a1d965e6a63b0523af4881214c5ec6d4f
phca <pedrocoli_andrade@hotmail.com>
2025-04-10 17:13:24 -0300

    variables starting stud

--- 1-pacotes/main.go
+++ 1-pacotes/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"pacotes-modulo/helper"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writing from main ")
 	helper.Write()
 	helper.Write2()
+	erro := checkmail.ValidateFormat("phcadev@gmail.com")
+	fmt.Println(erro)
 }
