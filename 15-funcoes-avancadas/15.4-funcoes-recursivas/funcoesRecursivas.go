ffab305084263ad8b99b2b4f2d6c4ce18fed9535
phca <pedrocoli_andrade@hotmail.com>
2025-04-29 10:48:40 -0300

    recursives functions go

---
+++ 15-funcoes-avancadas/15.4-funcoes-recursivas/funcoesRecursivas.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func Fibonacci(n uint) uint {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+func main() {
+	fmt.Println("Funções Recursivas")
+	// Chama a função recursiva
+	numero := uint(10)
+	fmt.Println("-------")
+	fmt.Printf("O %dº número de Fibonacci é: %d\n", numero, Fibonacci(numero))
+	fmt.Println("-------")
+}
